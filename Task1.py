adc2d7be90d0dfeab0211f825cbe0fe7e733c1f9
ayeshataj94 <ayeshataj94@gmail.com>
2025-08-17 00:49:26 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+operant_1 =int(input("Enter a number:"))
+operant_2=int(input("Enter a number:"))
+
+print("Addition:",operant_1+operant_2)
+print("Subtraction:",operant_1+operant_2)
+print("Multiplication:",operant_1*operant_2)
+print("Division:",operant_1/operant_2)
+
+
+
+
